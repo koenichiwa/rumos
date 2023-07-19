250b48c4f8a8c5fd1c3bff61e15c20ff43d88c93
koenichiwa <47349524+koenichiwa@users.noreply.github.com>
2023-07-19 16:35:27 +0200

    Refactor src/funcs.rs
    Removed `mod control_funcs`
    Removed ExitCode from result
    Split increase_or_decrease_brightness
    Moved match from `fn main_launch` to `fn change_brightness`

--- src/funcs/mod.rs
+++ src/funcs/mod.rs
@@ -1,89 +1,98 @@
-pub mod control_funcs {
-    use crate::args::{Cli, SetArgs};
-    use brightness::Brightness;
-    use colored::*;
-    use futures::TryStreamExt;
-    use std::process::ExitCode;
+use crate::args::{Cli, SetArgs, Commands};
+use brightness::Brightness;
+use colored::*;
+use futures::TryStreamExt;
 
-    pub async fn set_brightness(args: &SetArgs) -> Result<ExitCode, brightness::Error> {
-        brightness::brightness_devices()
-            .try_for_each(|mut dev| async move {
-                if args.percent < 5 {
-                    dev.set(5).await?;
+const MAX_BRIGHTNESS: u32 = 100;
+const MIN_BRIGHTNESS: u32 = 5;
+
+pub async fn change_brightness(cli: Cli) -> Result<(), brightness::Error> {
+    match cli.command {
+        Commands::Get => {},
+        Commands::Set(args) => set_brightness(brightness::brightness_devices(), args.percent).await?,
+        Commands::Inc(_) => increase_brightness(brightness::brightness_devices(), args.percent).await?,
+        Commands::Dec(_) => decrease_brightness(brightness::brightness_devices(), args.percent).await?,
+        Commands::Max => set_brightness(brightness::brightness_devices(), MAX_BRIGHTNESS).await?,
+        Commands::Min => set_brightness(brightness::brightness_devices(), MIN_BRIGHTNESS).await?,
+    }
+    print_brightness(brightness::brightness_devices(), cli)
+}
+
+pub async fn set_brightness(devices: dyn Stream<Item = Result<BrightnessDevice, Error>>, percentage: u32) -> Result<(), brightness::Error> {
+    brightness::brightness_devices()
+        .try_for_each(|mut device| async move {
+            if percentage < 5 {
+                device.set(5).await?;
+            } else {
+                device.set(percentage).await?;
+            }
+            Ok(())
+        }).await?;
+    Ok(())
+}
+
+pub async fn increase_brightness(devices: dyn Stream<Item = Result<BrightnessDevice, Error>>, percentage: u32) -> Result<(), brightness::Error>{
+    devices.try_for_each(|device| async move {
+        let level = device.get().await?;
+        if level + percentage < MAX_BRIGHTNESS {
+            dev.set(level + percentage).await?;
+        } else {
+            dev.set(MAX_BRIGHTNESS).await?;
+        }
+        Ok(())
+    }).await?;
+    Ok(())
+}
+
+pub async fn decrease_brightness(devices: dyn Stream<Item = Result<BrightnessDevice, Error>>, percentage: u32) -> Result<(), brightness::Error>{
+    devices.try_for_each(|device| async move {
+        let level = device.get().await?;
+        if level - percentage < MIN_BRIGHTNESS {
+            dev.set(level - percentage).await?;
+        } else {
+            dev.set(MIN_BRIGHTNESS).await?;
+        }
+        Ok(())
+    }).await?;
+    Ok(())
+}
+
+pub async fn print_brightness(devices: dyn Stream<Item = Result<BrightnessDevice, Error>>, cli: Cli) -> Result<(), brightness::Error> {
+    let _ = brightness::brightness_devices()
+        .try_for_each(|dev| async move {
+            let (device, result) = (dev.device_name().await?, dev.get().await?);
+            if !cli.quiet && !cli.percent {
+                if result >= 100 {
+                    println!(
+                        "{} brightness level reached ({})",
+                        "Maximum".green().bold(),
+                        "100%".green().bold()
+                    );
                     return Ok(());
                 }
-                let _ = dev.set(args.percent).await?;
-                Ok(())
-            })
-            .await?;
-        Ok(ExitCode::SUCCESS)
-    }
-
-    pub async fn increase_or_decrease_brightness(
-        args: &SetArgs,
-        mode: &str,
-    ) -> Result<ExitCode, brightness::Error> {
-
-        brightness::brightness_devices()
-            .try_for_each(|mut dev| async move {
-                let level = dev.get().await?;
-                if mode == "inc" {
-                    if level < 100 {
-                        dev.set(level + args.percent).await?;
-                    } else {
-                        return Ok(());
-                    }
-                } else {
-                    let calculate_value = dev.get().await? < (args.percent + 5);
-                    if calculate_value {
-                        dev.set(5).await?;
-                    } else {
-                        dev.set(level - args.percent).await?;
-                    }
-                };
-                Ok(())
-            })
-            .await?;
-        Ok(ExitCode::SUCCESS)
-    }
-
-    pub async fn print_brightness_lelel(cli: Cli) -> Result<ExitCode, brightness::Error> {
-        let _ = brightness::brightness_devices()
-            .try_for_each(|dev| async move {
-                let (device, result) = (dev.device_name().await?, dev.get().await?);
-                if !cli.quiet && !cli.percent {
-                    if result >= 100 {
-                        println!(
-                            "{} brightness level reached ({})",
-                            "Maximum".green().bold(),
-                            "100%".green().bold()
-                        );
-                        return Ok(());
-                    }
-                    if result <= 5 {
-                        println!(
-                            "{} brightness level reached ({})",
-                            "Minimum".red().bold(),
-                            "5%".red().bold()
-                        );
-                        return Ok(());
-                    }
-                }
-                if cli.quiet {
+                if result <= 5 {
+                    println!(
+                        "{} brightness level reached ({})",
+                        "Minimum".red().bold(),
+                        "5%".red().bold()
+                    );
                     return Ok(());
                 }
-                if cli.percent {
-                    println!("{}", format!("{result}%").yellow().bold());
-                    return Ok(());
-                }
-                println!(
-                    "Brightness of device {} is {}",
-                    device.blue().bold(),
-                    format!("{result}%").yellow().bold()
-                );
-                Ok(())
-            })
-            .await;
-        Ok(ExitCode::SUCCESS)
-    }
+            }
+            if cli.quiet {
+                return Ok(());
+            }
+            if cli.percent {
+                println!("{}", format!("{result}%").yellow().bold());
+                return Ok(());
+            }
+            println!(
+                "Brightness of device {} is {}",
+                device.blue().bold(),
+                format!("{result}%").yellow().bold()
+            );
+            Ok(())
+        })
+        .await;
+    Ok(())
 }
--- src/setup/mod.rs
+++ src/setup/mod.rs
@@ -1,47 +1,12 @@
 pub mod main_mod {
     use crate::args::{Cli, Commands};
-    use crate::funcs::control_funcs;
+    use crate::funcs;
     use brightness::Brightness;
     use clap::Parser;
     use futures::TryStreamExt;
 
     pub async fn main_launch() -> Result<(), brightness::Error> {
         let cli = Cli::parse();
-        match &cli.command {
-            Commands::Get => {
-                control_funcs::print_brightness_lelel(cli).await?;
-            }
-            Commands::Set(args) => {
-                control_funcs::set_brightness(args).await?;
-                control_funcs::print_brightness_lelel(cli).await?;
-            }
-            Commands::Inc(args) => {
-                control_funcs::increase_or_decrease_brightness(args, "inc").await?;
-                control_funcs::print_brightness_lelel(cli).await?;
-            }
-            Commands::Dec(args) => {
-                control_funcs::increase_or_decrease_brightness(args, "dec").await?;
-                control_funcs::print_brightness_lelel(cli).await?;
-            }
-            Commands::Max => {
-                let _ = brightness::brightness_devices()
-                    .try_for_each(|mut dev| async move {
-                        let _ = dev.set(100).await?;
-                        Ok(())
-                    })
-                    .await;
-                control_funcs::print_brightness_lelel(cli).await?;
-            }
-            Commands::Min => {
-                brightness::brightness_devices()
-                    .try_for_each(|mut dev| async move {
-                        dev.set(5).await?;
-                        Ok(())
-                    })
-                    .await?;
-                control_funcs::print_brightness_lelel(cli).await?;
-            }
-        }
-        Ok(())
+        change_brightness(cli).await?
     }
 }
